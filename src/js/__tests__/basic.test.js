d9210cd428d69b82e7b9d8898e2a3f30d77b78f9
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2020-09-02 22:20:39 +0300

    roblem with jest and throw

--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -1,7 +1,21 @@
-import sum from '../basic';
+import Character from '../app';
 
-test('should sum', () => {
-  const result = sum([1, 2, 3]);
-
-  expect(result).toBe(6);
+describe('check class Character', () => {
+  test('check return this of Character', () => {
+    const expected = {
+      level: 2,
+      attack: 12,
+      defence: 12,
+      health: 100,
+    };
+    const test1 = new Character(1, 10, 10, 20);
+    test1.levelup();
+    const received = test1;
+    expect(received).toEqual(expected);
+  });
+  test('check return Error if health = 0', () => {
+    const test1 = new Character(1, 10, 10, 0);
+    const received = test1.levelup();
+    expect(received).toThrowError('Character has dead :(');
+  });
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,31 @@
 // TODO: write your code here
-import sum from './basic';
 
-console.log('worked');
+class Character {
+  constructor(level, attack, defence, health) {
+    this.level = level;
+    this.attack = attack;
+    this.defence = defence;
+    this.health = health;
+  }
 
-console.log(sum([1, 2]));
+  levelup() {
+    try {
+      if (this.health <= 0) throw new Error();
+      this.level += 1;
+      this.attack += ((this.attack / 100) * 20);
+      this.defence += ((this.defence / 100) * 20);
+      this.health = 100;
+    } catch (e) {
+      throw new Error('Character has dead :(');
+    }
+  }
+}
+
+export default Character;
+
+const test1 = new Character(1, 10, 10, 10);
+console.log(test1);
+// Character {level: 1, attack: 10, defence: 10, health: 20}
+test1.levelup();
+console.log(test1);
+// Character {level: 2, attack: 12, defence: 12, health: 100}
